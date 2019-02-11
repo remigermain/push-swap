7587f185af452d7e390219272ab8d644c14d3c27
rgermain <germain.remi@protonmail.com>
2019-02-11 16:06:39 +0100

    i dont know

--- srcs/ps_algo.c
+++ srcs/ps_algo.c
@@ -6,7 +6,7 @@
 /*   By: rgermain <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/02/01 11:55:41 by rgermain     #+#   ##    ##    #+#       */
-/*   Updated: 2019/02/11 14:04:29 by rgermain    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/02/11 16:06:20 by rgermain    ###    #+. /#+    ###.fr     */
 /*   Updated: 2019/02/11 11:47:07 by rgermain    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
@@ -21,6 +21,8 @@ static void	find_mid_med(t_pusw *lst)
 	int		i;
 
 	val = 8;
+	if (val > (lst->len_a / (val / 2)))
+		val = 2;
 	min = find_min(lst->stack_a, lst->len_a);
 	i = 0;
 	while (i++ < ((lst->len_a / val) + (lst->len_a % val)))
@@ -31,12 +33,12 @@ static void	find_mid_med(t_pusw *lst)
 static void	split_stack(t_pusw *lst, char sens)
 {
 	int	val;
+	int	min;
 
 	val = 8;
 	ps_visu(lst);
 	find_mid_med(lst);
 	sens = find_midsens(lst);
-	sens = 1;
 	while (!sort_realstack_a(lst) && lst->len_a != -1)
 	{
 		if (((lst->len_a + lst->len_b + 2) < val) && lst->len_a > 0 && lst->stack_a[lst->len_a] > lst->stack_a[lst->len_a - 1] &&
@@ -52,20 +54,18 @@ static void	split_stack(t_pusw *lst, char sens)
 			if (lst->stack_b[lst->len_b] == lst->med)
 				find_mid_med(lst);
 			sens = find_midsens(lst);
-			sens = 1;
+			//sens = 1;
 		}
 		else if (sens == 1 && lst->stack_b > 0 &&
 				lst->stack_b[lst->len_b] < lst->stack_b[lst->len_b - 1])
 			rotate_ab(lst);
-		else if (sens == 1)
-			rotate_a(lst);
 		else
-			rev_rotate_a(lst);
+			rotate_a(lst);
 		ps_visu(lst);
 	}
 }
 
-static void	put_rest(t_pusw *lst, char sens, char ind_m, char ind_nm)
+static void	push_final(t_pusw *lst, char sens, char ind_m, char ind_nm)
 {
 	lst->max = find_max(lst->stack_b, lst->len_b);
 	lst->max_n = find_next_max(lst->stack_b, lst->len_b, lst->max);
@@ -106,7 +106,7 @@ void		ps_algo(t_pusw *lst)
 	if (!ft_issort(lst))
 	{
 		split_stack(lst, 0);
-		put_rest(lst, 0, 0, 0);
+		push_final(lst, 0, 0, 0);
 	}
 	ps_visu(lst);
 }
--- srcs/ps_algo_find.c
+++ srcs/ps_algo_find.c
@@ -6,7 +6,7 @@
 /*   By: rgermain <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/02/01 11:55:41 by rgermain     #+#   ##    ##    #+#       */
-/*   Updated: 2019/02/11 13:52:12 by rgermain    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/02/11 16:06:23 by rgermain    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -59,13 +59,13 @@ int	find_midsens(t_pusw *lst)
 		count++;
 	while (count2 >= 0 && lst->stack_a[count2] > lst->med)
 		count2--;
-	if (((lst->len_a - count2) + 1) == count)
+	if ((lst->len_a - count2) + 1 == count)
 	{
 		if (lst->stack_a[count] < lst->stack_a[count2])
 			return (1);
 		return (0);
 	}
-	else if (((lst->len_a - count2) + 1) > count)
+	else if ((lst->len_a - count2) + 1 > count)
 		return (0);
 	return (1);
 }
